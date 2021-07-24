2b3a39ce4b5872ae33b91ccbe9edbeaec48d7885
OrangeSea7 <87416321+OrangeSea7@users.noreply.github.com>
2021-07-24 21:25:25 +0800

    初识C语言
    1.变量和常量

---
+++ exe3.c
@@ -0,0 +1,74 @@
+定义变量的方法
+//
+//int age = 150;
+//float weight = 45.5f;
+//char ch = 'w';
+//
+变量的分类  （局部变量和全局变量）
+//#include <stdio.h>
+//
+//int global = 2019;//全局变量
+//int main()
+//
+//{
+//	int local = 2018;//局部变量
+//
+//	int global = 2020;//局部变量
+//	printf("global = %d\n",global);
+//
+//	return 0;
+//
+//
+//
+//}
+// 
+//
+变量的使用
+//#include <stdio.h>
+//int main()
+//{
+//	int num1 = 0;
+//	int num2 = 0;
+//	int sum = 0;
+//
+//	printf("输入两个操作数");
+//	scanf("%d %d",&num1,&num2);  //scanf是输出语句
+//	sum = num1 + num2;
+//	printf("sum=%d\n",sum);
+//	return 0;
+//
+
+常量  
+//#include <stdio.h>
+//
+//
+//	 enmu Sex
+//	{
+//		MALE,
+//		FEMALE,
+//		SECRET
+//
+//	};
+//括号中的MALE,FEMALE,SECRET是枚举常量
+
+
+	 //int main()
+	 //{
+		// 3.14;//字面常量
+		// 1000;//字面常量
+		// const float pai = 3.14f;   //const修饰的常量
+		// pai = 5014; //ok?
+  //       #define MAX 100          //#define的标识符常量
+		// return 0;
+	 //
+	 //
+	 //
+	 //
+	 //}
+
+
+
+
+
+
+
